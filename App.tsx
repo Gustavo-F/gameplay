51b8c6989c0487dd38e5afc30ca5a5b43eb0f101
Gustavo-F <fgustavo016@gmail.com>
2021-11-11 11:03:55 -0300

    Added context auth to app routes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { useFonts } from 'expo-font';
 import { StatusBar } from "react-native";
+import AppLoading from 'expo-app-loading';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
-import AppLoading from 'expo-app-loading';
-import { useFonts } from 'expo-font';
 
 import { Routes } from './src/routes';
+import { AuthProvider } from "./src/hooks/auth";
 import { Background } from "./src/components/Background";
 
 export default function App() {
@@ -27,7 +28,9 @@ export default function App() {
 				backgroundColor='transparent' 
 				translucent 
 			/>
-			<Routes />
+			<AuthProvider>
+				<Routes />
+			</AuthProvider>
 		</Background>
 	);
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { useNavigation } from '@react-navigation/native'
 
-import IllustrationImg from '../../assets/illustration.png'; 
 import { styles } from './styles';
-
+import { useAuth } from "../../hooks/auth";
 import { ButtonICon } from "../../components/ButtonIcon";
+import IllustrationImg from '../../assets/illustration.png'; 
+
 
 export function SignIn() {
     const navigation = useNavigation();
-    
+    const { user } = useAuth();
+
     function handleSignIn() {
         navigation.navigate('Home');
     }
